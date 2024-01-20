2c1e7dbde0bdde891e191a9114645ee1fdff8ddd
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2024-01-20 21:48:35 +0530

    Update AI-ANALYZER.py

--- AI-ANALYZER.py
+++ AI-ANALYZER.py
@@ -20,7 +20,7 @@ st.markdown("""
             </li>
             <li>
                 <div>
-                    <a href="#" target="_self"><button style="background-color: #fff; color: #443e85; padding: 0.5rem 1rem; border: none; cursor: pointer; border-radius: 1rem; margin-top: 3px;">Close</button></a>
+                    <a href="https://hdaanalyzer.000webhostapp.com/ai-analyzer.php" target="_self"><button style="background-color: #fff; color: #443e85; padding: 0.5rem 1rem; border: none; cursor: pointer; border-radius: 1rem; margin-top: 3px;">Close</button></a>
                 </div>
             </li>
         </ul>
